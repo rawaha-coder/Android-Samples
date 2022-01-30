ec62cbc2bc9562b26f0a2736ef3deb9a2e19a8dd
rawaha-coder <rawaha.coder@gmail.com>
2022-01-30 22:46:24 +0100

    add radio button, FAB and others views to Bits and Pizzas App

--- BitsAndPizzas/app/src/main/java/com/hybcode/bitsandpizzas/OrderFragment.kt
+++ BitsAndPizzas/app/src/main/java/com/hybcode/bitsandpizzas/OrderFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.chip.Chip
 
 
 class OrderFragment : Fragment() {
@@ -14,12 +17,34 @@ class OrderFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         val view = inflater.inflate(R.layout.fragment_order, container, false)
 
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
-
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizza_group)
+
+        val pizzaType = pizzaGroup.checkedRadioButtonId
+
+        if (pizzaType == -1){
+
+        }else{
+            val radio = view.findViewById<RadioButton>(pizzaType)
+        }
+
+        val parmesan = view.findViewById<Chip>(R.id.parmesan)
+
+        if (parmesan.isChecked) {
+
+        }
+
+        val chiliOil = view.findViewById<Chip>(R.id.chili_oil)
+
+        if (chiliOil.isChecked){
+
+        }
+
         return view
     }
 }
